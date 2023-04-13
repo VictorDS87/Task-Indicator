ff42bc90deb5a533a64adb119cfe64b98c6927f6
Victor Donizete <victordonizete65@gmail.com>
2023-04-13 11:35:07 -0300

    global style

---
+++ src/styles/global.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: 'Inter', sans-serif;
+
+        background-color: #1A1A1A;
+    }
+`
